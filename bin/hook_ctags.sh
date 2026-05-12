943a9db0aeecc50034475eee103695d10a24c199
Renato Botelho <garga@FreeBSD.org>
2026-05-12 14:27:19 +0000

    hook_ctags: Cleanup temporary files

--- bin/hook_ctags.sh
+++ bin/hook_ctags.sh
@@ -16,6 +16,6 @@ fi
 set -e
 PATH="/opt/local/bin:$PATH"
 git_dir=$(git rev-parse --git-dir)
-trap "rm -f ${git_dir}/tags.$$" EXIT
+trap "rm -f ${git_dir}/tags.$$" HUP INT TERM ERR EXIT
 $ctags --tag-relative -Rf ${git_dir}/tags.$$ --exclude=.git $params >/dev/null 2>&1
 mv ${git_dir}/tags.$$ ${git_dir}/tags
